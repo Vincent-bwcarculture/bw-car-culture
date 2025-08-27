ed7b1f76e6a485cdb99ecb2e1566c3d45e0722f8
Vincent Katso <kvramphotho@gmail.com>
2025-08-28 00:49:40 +0200

    business card pin removal

--- client/src/components/shared/BusinessCard/BusinessCard.js
+++ client/src/components/shared/BusinessCard/BusinessCard.js
@@ -227,7 +227,7 @@ const BusinessCard = ({ business, onAction, compact = false }) => {
               
               {business.location && (
                 <div className="bcc-business-location">
-                  <span className="bcc-location-icon">📍</span>
+                  <span className="bcc-location-icon"></span>
                   <span>
                     {business.location?.city || 'Unknown'}
                     {business.location?.country ? `, ${business.location.country}` : ''}
